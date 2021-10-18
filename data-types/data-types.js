f7f0397bf69515a924d34c77ac2720f373cac6c2
Rosa Marquez <rmarquez101@cnm.edu>
2021-10-18 09:51:08 -0600

    m

---
+++ data-types/data-types.js
@@ -0,0 +1,17 @@
+// string
+const name = 'fred'
+const anotherName = "jim"
+const yetAnotherName = `mary and ${name}`
+
+// number
+const age =  18
+
+const zipcode = '87109'
+
+const cars = ['ford', 'dodge', 'toyota']
+const empty = []
+console.log(cars[0])
+console.log(cars[2])
+
+// object ... json
+const person = {name: 'fred', age: 18}  //{ means object }
---
+++ function-2/arrow-functions.js
@@ -0,0 +1,14 @@
+
+function myFunction (argument) {
+    console.log(argument)
+}
+
+myOtherFunction = function (argument){
+    console.log(argument)
+}
+
+myArrayFunction = (argument) => {
+    console.log(argument)
+}
+
+myShorterArrowFunction = argument => console.log(argument)
